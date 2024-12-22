e2f611fa9b475b84df54ee98ac537eba3e637a10
Binyaminf <binyamin.filtser@mobileye.com>
2024-12-22 16:51:04 +0200

    Update TaskList.tsx
    Add task edit

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,5 @@
 import { TaskCard, Task } from "./TaskCard";
+import { useNavigate } from 'react-router-dom';
 
 interface TaskListProps {
   tasks: Task[];
@@ -6,13 +7,20 @@ interface TaskListProps {
 }
 
 export function TaskList({ tasks, onTaskClick }: TaskListProps) {
+  const navigate = useNavigate();
+
+  const handleTaskClick = (task: Task) => {
+    onTaskClick?.(task);
+    navigate(`/edit/${task.id}`);
+  };
+
   return (
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       {tasks.map((task) => (
         <TaskCard
           key={task.id}
           task={task}
-          onClick={() => onTaskClick?.(task)}
+          onClick={() => handleTaskClick(task)}
         />
       ))}
     </div>
